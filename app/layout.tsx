412c0ef42ed5f0cddfe127dd26e5a59fa72c87d1
jacobSalazku <Cooljss10@gmail.com>
2026-04-02 13:04:17 +0200

    fix: hydration warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Providers } from './providers';
-import '@/styles/globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        suppressHydrationWarning
+        className={`${geistSans.variable} ${geistMono.variable}`}
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
