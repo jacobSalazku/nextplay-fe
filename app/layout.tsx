b0fd9dacd06c5558ac226579a2901b88d25927b1
jacobSalazku <Cooljss10@gmail.com>
2026-05-23 06:41:23 +0200

    refactor: update layout to use new font styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
+import { righteous, roboto } from '@/styles/font';
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import { Providers } from './providers';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -27,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable}`}
+        className={`${righteous.variable} ${roboto.variable}`}
       >
         <Providers>{children}</Providers>
       </body>
